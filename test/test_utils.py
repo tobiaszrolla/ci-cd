1fa22821a2f0c2b2d5c764b180e899e56fb5ea6f
Tobi <rollatobiasz@gmail.com>
2025-06-09 10:07:36 +0200

    test path problem

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,4 @@
-from src import utils
+from src.utils import add, sub, mult, div
 import pytest
 import utils
 
@@ -6,26 +6,26 @@ import utils
 5 ) , (3 , 4 , 7 ) , (4 , -5 , -1 )])
 
 def test_add(a , b , expected ):
-    result = utils.add(a , b )
+    result = add(a , b )
     assert result == expected
 
 @pytest.mark.parametrize ("a, b, expected ", [(5 , 2 , 3 ) , (1 , 3 ,
 -2 ) , (145 , 20 , 125 ) , (2 , 15 , -13 )])
 
 def test_sub(a , b , expected ):
-    result = utils.sub(a , b )
+    result = sub(a , b )
     assert result == expected
 
 @pytest.mark.parametrize ("a, b, expected ", [(7, 8, 56 ) , (2 , 3 ,
  6) , (3 , 7 , 21 ) , (-2 , 5 , -10 )])
 
 def test_mult(a , b , expected ):
-    result = utils.mult(a , b )
+    result = mult(a , b )
     assert result == expected
 
 @pytest.mark.parametrize ("a, b, expected ", [(2, 1, 2 ) , (5 , 2 ,
  2.5) , (1 , -4, -0.25 ) , (1 , 1 , 1 )])
 
 def test_div(a , b , expected ):
-    result = utils.div(a , b )
+    result = div(a , b )
     assert result == expected

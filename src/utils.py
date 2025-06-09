c7f9aa61d58fddfdad3fd61f915eec0263bda032
Tobi <rollatobiasz@gmail.com>
2025-06-09 10:30:22 +0200

    black formated

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,20 @@
 """Basic arithmetic utility functions."""
 
+
 def add(a: int, b: int) -> int:
     """Return the sum of a and b."""
     return a + b
+
 
 def sub(a: int, b: int) -> int:
     """Return the difference of a and b."""
     return a - b
 
+
 def mult(a: int, b: int) -> int:
     """Return the product of a and b."""
     return a * b
+
 
 def div(a: int, b: int) -> float:
     """Return the quotient of a divided by b."""

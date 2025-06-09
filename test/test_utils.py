546d56427ef27af19c41742b151e6d65c01ac951
Tobi <rollatobiasz@gmail.com>
2025-06-09 10:01:51 +0200

    first commit issue

---
+++ test/test_utils.py
@@ -0,0 +1,31 @@
+import src
+import pytest
+import utils
+
+@pytest.mark.parametrize ("a, b, expected ", [(1 , 2 , 3 ) , (2 , 3 ,
+5 ) , (3 , 4 , 7 ) , (4 , -5 , -1 )])
+
+def test_add(a , b , expected ):
+    result = utils.add(a , b )
+    assert result == expected
+
+@pytest.mark.parametrize ("a, b, expected ", [(5 , 2 , 3 ) , (1 , 3 ,
+-2 ) , (145 , 20 , 125 ) , (2 , 15 , -13 )])
+
+def test_sub(a , b , expected ):
+    result = utils.sub(a , b )
+    assert result == expected
+
+@pytest.mark.parametrize ("a, b, expected ", [(7, 8, 56 ) , (2 , 3 ,
+ 6) , (3 , 7 , 21 ) , (-2 , 5 , -10 )])
+
+def test_mult(a , b , expected ):
+    result = utils.mult(a , b )
+    assert result == expected
+
+@pytest.mark.parametrize ("a, b, expected ", [(2, 1, 2 ) , (5 , 2 ,
+ 2.5) , (1 , -4, -0.25 ) , (1 , 1 , 1 )])
+
+def test_div(a , b , expected ):
+    result = utils.div(a , b )
+    assert result == expected
